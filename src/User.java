b82b906531590e8fe2d363eedf6785bc6ca36317
soppitopp <soppitopp2000@hotmail.com>
2025-10-19 22:55:33 +0200

    add: boolean isContainingInvalidCharacters(String input) + content

--- src/User.java
+++ src/User.java
@@ -1,6 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class User {
     protected boolean isContainingNumber(String input) {
         String containsNumber = "[0123456789]";
         return containsNumber.indexOf(input.charAt(0)) >= 0;
+    }
+
+    protected boolean isContainingInvalidCharacters(String input) {
+        Pattern patternForSpecialCharacters = Pattern.compile(".[!,.*+/|<>#¤%&=?@£${}_].");
+        Matcher matcherForSpecialCharacters = patternForSpecialCharacters.matcher(input);
+        return matcherForSpecialCharacters.find();
     }
 }
