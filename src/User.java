66fadadfae72d734a60341321c552301bd5dc6c2
soppitopp <soppitopp2000@hotmail.com>
2025-10-19 22:39:47 +0200

    add: isContainingNumber(String input)

---
+++ src/User.java
@@ -0,0 +1,6 @@
+public class User {
+    private boolean isContainingNumber(String input) {
+        String containsNumber = "[0123456789]";
+        return containsNumber.indexOf(input.charAt(0)) >= 0;
+    }
+}
