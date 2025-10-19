ab8266c860757fb6c13170c42d7c29e63439244c
soppitopp <soppitopp2000@hotmail.com>
2025-10-19 11:36:12 +0200

    add: Enum MemberType + content

---
+++ src/MemberType.java
@@ -0,0 +1,11 @@
+public enum MemberType {
+    GOLD("Gold"),
+    PLATINUM("Platinum"),
+    STANDARD("Standard");
+
+    public final String memberType;
+
+    MemberType(String memberType) {
+        this.memberType = memberType;
+    }
+}
