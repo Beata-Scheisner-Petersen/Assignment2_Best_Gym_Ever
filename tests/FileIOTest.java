78dc91db4d522b2d4b4c23fc319aace63e78a60f
soppitopp <soppitopp2000@hotmail.com>
2025-10-20 15:46:02 +0200

    add: checkIfContentIsCorrectTest() test: fail

--- tests/FileIOTest.java
+++ tests/FileIOTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 public class FileIOTest {
     FileIO fileHandler = new FileIO();
 
@@ -9,5 +8,12 @@ public class FileIOTest {
     public void checkReadFilePathTest() {
         assertTrue(fileHandler.checkReadFilePath(fileHandler.readFilePath));
         assertFalse(!fileHandler.checkReadFilePath(fileHandler.readFilePath));
+    }
+
+    @Test
+    public void checkIfContentIsCorrectTest() {
+        assertEquals("Fredrik Berggren", fileHandler.checkIfContentIsCorrect("Fredrik Berggren"));
+        assertEquals("Fredrik Berggren", fileHandler.checkIfContentIsCorrect("361026-1822"));
+        assertNotEquals("Fredrika Berggren", fileHandler.checkIfContentIsCorrect("Fredrik Berggren"));
     }
 }
