4ba30287064d5f700f900fa04dd66a2efb2a6a77
soppitopp <soppitopp2000@hotmail.com>
2025-10-20 16:07:36 +0200

    fix: Change in code I forget to fix in test test: passed

--- tests/UserTest.java
+++ tests/UserTest.java
@@ -29,10 +29,9 @@ public class UserTest {
         assertFalse(user.isContainingInvalidCharacters("abrâ"));
     }
     @Test
-    public void hyphenCounterTest() {
-        assertEquals(1, user.isHyphenOnlyOneOf("860923-1258"));
-        assertNotEquals(0, user.isHyphenOnlyOneOf("860923-1258"));
-        assertNotEquals(1, user.isHyphenOnlyOneOf("860-23-1258"));
+    public void isHyphenOnlyOneOfTest() {
+        assertTrue(user.isHyphenOnlyOneOf("860923-1258"));
+        assertFalse(user.isHyphenOnlyOneOf("860-23-1258"));
     }
 
     @Test
