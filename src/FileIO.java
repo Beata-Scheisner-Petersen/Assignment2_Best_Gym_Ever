1200faafeb45c4d354200dd7c233263f0110d67c
soppitopp <soppitopp2000@hotmail.com>
2025-10-20 15:59:16 +0200

    add: checkIfContentIsCorrect()

--- src/FileIO.java
+++ src/FileIO.java
@@ -18,7 +18,28 @@ public class FileIO {
             IO.println("Error:");
             e.printStackTrace();
         }
-
         return pathValid;
+    }
+
+    protected boolean checkIfContentIsCorrect(String readFilePath, String input) {
+        boolean checkValid = false;
+        String temp;
+        try(BufferedReader br = new BufferedReader(new FileReader(readFilePath))) {
+            while ((temp = br.readLine()) != null) {
+                if (temp.contains(input)) {
+                    checkValid = true;
+                    break;
+                }
+            }
+        } catch (FileNotFoundException eFile) {
+            IO.println("File not found");
+        } catch (IOException eIO) {
+            IO.println("IO error");
+            eIO.printStackTrace();
+        } catch (Exception e) {
+            IO.println("Error:");
+            e.printStackTrace();
+        }
+        return checkValid;
     }
 }
