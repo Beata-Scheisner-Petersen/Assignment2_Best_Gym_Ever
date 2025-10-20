b7ba04bde378e341686e844fe1c3f4948c207c91
soppitopp <soppitopp2000@hotmail.com>
2025-10-20 15:33:43 +0200

    add: FileIO + checkReadFilePath

---
+++ src/FileIO.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+public class FileIO {
+    protected String readFilePath = "gym_medlemmar.txt";
+
+    protected boolean checkReadFilePath(String readFilePath) {
+        boolean pathValid = false;
+        try(BufferedReader br = new BufferedReader(new FileReader(readFilePath))) {
+            pathValid = true;
+        } catch (FileNotFoundException eFile) {
+            IO.println("File not found");
+        } catch (IOException eIO) {
+            IO.println("IO error");
+            eIO.printStackTrace();
+        } catch (Exception e) {
+            IO.println("Error:");
+            e.printStackTrace();
+        }
+
+        return pathValid;
+    }
+}
