7764b204052ebf0684af09f57d9d046cf1e5b370
soppitopp <soppitopp2000@hotmail.com>
2025-10-21 21:12:13 +0200

    fix: change name: fileHandler to ioh

--- tests/InputOutputHandlerTest.java
+++ tests/InputOutputHandlerTest.java
@@ -1,28 +1,34 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 public class InputOutputHandlerTest {
-    InputOutputHandler fileHandler = new InputOutputHandler();
+    InputOutputHandler ioh = new InputOutputHandler();
     String readFilePath = "gym_medlemmar.txt";
 
     @Test
     public void checkReadFilePathTest() {
-        assertTrue(fileHandler.checkReadFilePath(readFilePath));
-        assertFalse(!fileHandler.checkReadFilePath(readFilePath));
+        assertTrue(ioh.checkReadFilePath(readFilePath));
+        assertFalse(!ioh.checkReadFilePath(readFilePath));
     }
 
     @Test
     public void checkIfContentIsCorrectTest() {
-        assertTrue(fileHandler.checkIfContentIsCorrect("Astrid Larsson"));
-        assertTrue(fileHandler.checkIfContentIsCorrect("540815-4382"));
-        assertFalse(fileHandler.checkIfContentIsCorrect("Fredrika Berggren"));
+        assertTrue(ioh.checkIfContentIsCorrect("Astrid Larsson"));
+        assertTrue(ioh.checkIfContentIsCorrect("540815-4382"));
+        assertFalse(ioh.checkIfContentIsCorrect("Fredrika Berggren"));
     }
 
     @Test
     public void findInFileTest() {
-        assertTrue(fileHandler.findInFile("Fredrik Berggren"));
-        assertTrue(fileHandler.findInFile("540815-4382"));
-        assertFalse(fileHandler.findInFile("540815-b382"));
+        assertTrue(ioh.findInFile("Fredrik Berggren"));
+        assertTrue(ioh.findInFile("540815-4382"));
+        assertFalse(ioh.findInFile("540815-b382"));
     }
+
+    @Test
+    public void getCustomerTypeTest() {
+        ioh.findInFile("Fredrik Berggren");
+        assertEquals("Platina", ioh.getCustomerType());
+    }
+
 }
