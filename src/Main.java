382c790bd6fd88073bfd7b09b4267c6bf9175824
soppitopp <soppitopp2000@hotmail.com>
2025-10-21 11:02:38 +0200

    fix: taking in input logic

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 void main() {
    User user = new User();
-   System.out.print("Enter name or social security number: ");
-   String temp = IO.readln();
-   if (user.isValidInput(temp)) {
-       user.setInput(temp);
+   String input;
+   while (true) {
+       System.out.print("Enter name or social security number or q to quit: ");
+       String temp = IO.readln().trim();
+       if (temp.equals("q")) {
+           break;
+       }else if (user.isValidInput(temp)) {
+           input = temp;
+           break;
+       }
    }
 }
