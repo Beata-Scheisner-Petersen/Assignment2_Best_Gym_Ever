a855218f195ec01eacb9efb8560946e5aa5a7ee6
soppitopp <soppitopp2000@hotmail.com>
2025-10-21 15:55:11 +0200

    remove: setInfo

--- src/IOUtil.java
+++ src/IOUtil.java
@@ -49,47 +49,26 @@ public class IOUtil {
     protected boolean findInFile(String input) {
         boolean findResult = false;
         if (checkReadFilePath(readFilePath) && checkIfContentIsCorrect(readFilePath, input)) {
-            try(Scanner reader = new Scanner(new FileReader(readFilePath))) {
 
-                if (setInfo(reader)) {
-                    if (person.getName().contains(input)){
-                        findResult = true;
+                try(Scanner reader = new Scanner(new FileReader(readFilePath))) {
+
+                    while (reader.hasNext()) {
+                        if (setInfo(reader)) {
+                            if (person.getName().contains(input.trim())){
+                                findResult = true;
+                                break;
+                            }
+                        }
                     }
+                } catch (Exception e) {
+                    IO.println("Error:");
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                IO.println("Error:");
-                e.printStackTrace();
-            }
+
         } else {
             IO.println("Something went wrong when trying to read the file");
         }
 
         return findResult;
-    }
-    protected boolean setInfo (Scanner reader) {
-        reader.skip("Namn;Adress;Mailadress;Personnummer;Datum_köpt_gymmedlemskap;Datum_senast_uppdaterad;Medlemsnivå");
-        reader.useDelimiter(";");
-        person.setName(reader.next());
-        person.setAdress(reader.next());
-        person.setMailadress(reader.next());
-        person.setPersonNumber(reader.next());
-        person.setDateStarted(LocalDate.parse(reader.next()));
-        person.setLatestBoughtMembership(LocalDate.parse(reader.next()));
-        String temp = reader.next();
-        if (temp.contains("Platina")) {
-            person.setMemberType(MemberType.PLATINUM);
-        } else if (temp.contains("Guld")) {
-            person.setMemberType(MemberType.GOLD);
-        } else if (temp.contains("Standard")) {
-            person.setMemberType(MemberType.STANDARD);
-        } else {
-            IO.println("Sorry error occurred couldn't get data");
-        }
-        if (person.getName() != null && person.getAdress() != null && person.getMailadress() != null && person.getPersonNumber() != null &&
-                person.getDateStarted().toString() != null && person.getLatestBoughtMembership().toString() != null && person.getMemberType() != null) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
