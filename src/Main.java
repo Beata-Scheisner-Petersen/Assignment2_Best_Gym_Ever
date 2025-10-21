6b88ee8c04ef06c145903502b56fd0bae590a9fc
soppitopp <soppitopp2000@hotmail.com>
2025-10-21 19:17:37 +0200

    fix: logic error

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 void main() {
    User user = new User();
-   String input;
+   InputOutputHandler ioh = new InputOutputHandler();
+   String input = null;
+   String temp;
    while (true) {
        System.out.print("Enter name or social security number or q to quit: ");
-       String temp = IO.readln().trim();
+       temp = IO.readln().trim();
        if (temp.equals("q")) {
            break;
        }else if (user.isValidInput(temp)) {
            input = temp;
            break;
+       } else {
+           IO.println("Invalid info");
        }
+   }
+   if ((!temp.equals("q")) && ioh.findInFile(input)) {
+       //
    }
 }
