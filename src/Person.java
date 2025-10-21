789ef35bd6ceccf04c7e18496d9868ef86e66751
soppitopp <soppitopp2000@hotmail.com>
2025-10-21 11:28:40 +0200

    add: adress + mailadress + dateStarted + latesBoughtMembership. fix: moved customerType + removed and add all getter and setters

--- src/Person.java
+++ src/Person.java
@@ -1,8 +1,13 @@
+import java.time.LocalDate;
 public class Person {
     private String name;
+    private String adress;
+    private String mailadress;
     private String personNumber;
-    private CustomerType customerType;
+    private LocalDate dateStarted;
+    private LocalDate latestBoughtMembership;
     private MemberType memberType;
+    private CustomerType customerType;
 
     public String getName() {
         return name;
@@ -12,12 +17,44 @@ public class Person {
         this.name = name;
     }
 
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getMailadress() {
+        return mailadress;
+    }
+
+    public void setMailadress(String mailadress) {
+        this.mailadress = mailadress;
+    }
+
     public String getPersonNumber() {
         return personNumber;
     }
 
     public void setPersonNumber(String personNumber) {
         this.personNumber = personNumber;
+    }
+
+    public LocalDate getDateStarted() {
+        return dateStarted;
+    }
+
+    public void setDateStarted(LocalDate dateStarted) {
+        this.dateStarted = dateStarted;
+    }
+
+    public LocalDate getLatestBoughtMembership() {
+        return latestBoughtMembership;
+    }
+
+    public void setLatestBoughtMembership(LocalDate latestBoughtMembership) {
+        this.latestBoughtMembership = latestBoughtMembership;
     }
 
     public CustomerType getCustomerType() {
